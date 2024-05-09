5bf4386bd5c412a0ed907e45ad5a5ef1000a7b20
Salma Torres <tsalma569@gmail.com>
2024-05-09 09:49:56 -0400

    Toma en cuenta una cadena con dos numeros

--- src/calculadora.js
+++ src/calculadora.js
@@ -3,6 +3,12 @@ function SumarCadena(cadenaDeNumeros){
     {
         return parseInt(cadenaDeNumeros);
     }
+    if(cadenaDeNumeros.length === 3)
+    {
+        const numero1 = parseInt(cadenaDeNumeros[0]);
+        const numero2 = parseInt(cadenaDeNumeros[2]);
+        return numero1 + numero2;
+    }
     return 0;
 }
 
--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -6,5 +6,8 @@ describe("Calculadora", () => {
     });
     it("Deberia devolver un numero cuando se ingresa una cadena con un solo numero", () => {
         expect(SumarCadena("2")).toEqual(2);
-      });
+    });
+    it("Deberia devolver la suma de dos numeros que estan en una cadena", () => {
+        expect(SumarCadena("1,2")).toEqual(3);
+    });
 });
